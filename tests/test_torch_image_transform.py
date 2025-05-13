dc0b51dfd53a1dd6fb6a03740f4fe1f9040d4224
Spencer J Rothfuss <108840392+sjrothfuss@users.noreply.github.com>
2025-05-13 13:31:02 -0500

    2D rot shift convenience layer (#10)
    * Starting shift_rotate_image_2d
    
    * Add test for shift_rotate_image_2d
    
    * Revert automatic reformatting by Black
    
    * Change to center rotation
    
    * Be explicit about only accepting single values
    
    * Correct operation order
    
    * Undo automatic black formatting
    
    * Undo automatic black formatting
    
    * Undo automatic black formatting
    
    * Add shift_rotate_image_2d to __all__
    
    * Move image_center out of affine_transform
    
    * Rename to rotate_shift_image_2d to match default order of operations
    
    * Doc string and simplifying
    
    * Reverse shift direction
    
    * Rename angle to rotate
    
    * Add warning about direction conventions
    
    * Create shift_then_rotate_image_2d
    
    ---------
    
    Co-authored-by: Johannes Elferich <jojotux123@hotmail.com>

--- tests/test_torch_image_transform.py
+++ tests/test_torch_image_transform.py
@@ -1,5 +1,6 @@
 import torch
 from torch_transform_image import affine_transform_image_2d, affine_transform_image_3d
+from torch_transform_image import rotate_then_shift_image_2d, shift_then_rotate_image_2d
 from torch_affine_utils.transforms_2d import T as T_2d, S as S_2d
 from torch_affine_utils.transforms_3d import T as T_3d, S as S_3d
 
@@ -71,6 +72,42 @@ def test_affine_transform_image_3d():
     assert result[18, 14, 14] == 0
 
 
+def test_rotate_shift_image_2d():
+    image = torch.zeros((28, 28), dtype=torch.float32)
+    image[18, 14] = 1
+    image = image.float()
+
+    result = rotate_then_shift_image_2d(
+        image=image,
+        rotate=90,
+        shift=[2, 0],
+        interpolation="bicubic",
+    )
+
+    # sanity check, array center which was 4 voxels below the dot should now be 1
+    assert image[12, 12] == 0
+    assert result[16, 10] == 1
+    assert result[18, 14] == 0
+
+
+def test_shift_rotate_image_2d():
+    image = torch.zeros((28, 28), dtype=torch.float32)
+    image[18, 14] = 1
+    image = image.float()
+
+    result = shift_then_rotate_image_2d(
+        image=image,
+        rotate=90,
+        shift=[2, 0],
+        interpolation="bicubic",
+    )
+
+    # sanity check, array center which was 4 voxels below the dot should now be 1
+    assert image[12, 12] == 0
+    assert result[14, 8] == 1
+    assert result[18, 14] == 0
+
+
 def test_affine_transform_image_3d_scaling():
     # set up test image with dot at (18, 14)
     image = torch.zeros((28, 28, 28), dtype=torch.float32)
@@ -90,4 +127,3 @@ def test_affine_transform_image_3d_scaling():
     assert result.shape == (56,56,56)
     assert result[36, 28, 28] == 1
     assert result[18, 14, 14] == 0
-
